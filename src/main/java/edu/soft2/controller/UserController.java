f3487b28738ecf310a7481fccc2db85a7c85ccfe
liubei <2635914904@qq.com>
2021-12-07 09:22:49 +0800

    变更

--- src/main/java/edu/soft2/controller/UserController.java
+++ src/main/java/edu/soft2/controller/UserController.java
@@ -5,13 +5,16 @@ import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
 import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.*;
+import java.util.Map;
 import java.util.UUID;
 
 @Controller
@@ -52,11 +55,15 @@ public class UserController {
         }
         return "hello";
     }
+
+
     @RequestMapping(value = "/hello")
     public String hello(){
         System.out.println("---login()---");
         return "hello";
     }
+
+
     @RequestMapping(value = "upload",method = {RequestMethod.POST})
     public String upload(MultipartFile image,HttpServletRequest request) throws IOException {
         System.out.println("---upload()--");
@@ -76,19 +83,26 @@ public class UserController {
         os.close();is.close();
         return "welcome";
     }
+
+
+
     @RequestMapping(value = "upload2",method = {RequestMethod.POST})
-    public String upload2(MultipartFile[] image,HttpServletRequest request) throws IOException {
+    public String upload2(MultipartFile[] image, HttpServletRequest request, HttpServletResponse response,Map<String ,Object> map) throws IOException {
         System.out.println("---upload2()--");
         InputStream is=null;
         OutputStream os=null;
-        String [] a={"1","2","3"};
+//        String [] a={"1","2","3"};
         for (MultipartFile imageFile:image) {
             System.out.println("UserController.upload2.foreach");
-//            System.out.println(imageFile);
             //获取文件输入流对象
           is=imageFile.getInputStream();
             //获取文件名称
             String filename=imageFile.getOriginalFilename();
+//           System.out.println("文件名"+filename);
+            if (filename.equals("")) {
+                System.out.println("空字符串，进入下一轮循环");
+                continue;//结束本轮循环进入下一轮循环
+            }
             //设置上传路径
             String realPath=request.getServletContext().getRealPath("/upload2");
             System.out.println("上传路径为"+realPath);
@@ -96,17 +110,35 @@ public class UserController {
             String newname=doFilename(filename);
             os=new FileOutputStream(new File(realPath,newname));
             int size= IOUtils.copy(is,os);//完成文件拷贝的大小(字节)
+            if (size>0) {
+                response.setContentType("text/html;charset=utf-8");
+                PrintWriter out=response.getWriter();
+                out.println("<script language=\"javascript\">alert('上传成功!');</script>");
             System.out.println("上传"+filename+"到("+realPath+")完毕,共计"+size+"字节，上传后的文件名为'"+newname+"'。");
-            //资源释放(先开后关，后开先关)
+            }
         }
+            //资源释放(先开后关，后开先关)
             os.close();is.close();
         return "welcome";
 
     }
+
+
     private String doFilename(String filename){
         //获取文件后缀名
         String extension= FilenameUtils.getExtension(filename);
         String uuid=UUID.randomUUID().toString();
         return uuid+"."+extension;//上传文件的新名称
+    }
+
+
+    @RequestMapping("/download.do/{filename}")
+    public void download(@PathVariable String filename,HttpServletRequest request,HttpServletResponse response) throws IOException {
+        String realPath=request.getServletContext().getRealPath("/images");
+        System.out.println("下载路径"+realPath);
+            InputStream is=new FileInputStream(new File(realPath,filename));
+            OutputStream os=response.getOutputStream();
+       IOUtils.copy(is,os);//拷贝
+       os.close();is.close();//io流
     }
 }
--- src/main/java/web/WEB-INF/pages/hello.jsp
+++ src/main/java/web/WEB-INF/pages/hello.jsp
@@ -18,6 +18,21 @@
         <input type="submit" value="批量上传">
     </form>
 </div>
+<div>
+    <h2>点击图片下载</h2>
+    <a href="${pageContext.request.contextPath}/user/download.do/blossom1.jpg">
+    <image src="${pageContext.request.contextPath}/images/blossom1.jpg" width="300px"></image>
+    </a>
+    <a href="${pageContext.request.contextPath}/user/download.do/blossom2.jpg">
+        <image src="${pageContext.request.contextPath}/images/blossom2.jpg" width="300px"></image>
+    </a>
+    <a href="${pageContext.request.contextPath}/user/download.do/Desert.jpg">
+        <image src="${pageContext.request.contextPath}/images/Desert.jpg" width="300px"></image>
+    </a>
+    <a href="${pageContext.request.contextPath}/user/download.do/菊花.jpg">
+        <image src="${pageContext.request.contextPath}/images/菊花.jpg" width="300px"></image>
+    </a>
+</div>
 <%--hello,${requestScope.username}!!! 密码是:${requestScope.age}你今年${requestScope.age}岁。--%>
 <%--hello,${sessionScope.username}!!!你今年${sessionScope.age}岁。--%>
 hello,${requestScope.user.username}!!!<br>
